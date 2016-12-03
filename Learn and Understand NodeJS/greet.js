c1f595269020d49fc5f643d364d35e1b56c81f14
Daniel Anthony Noventa <daniel.a.noventa@gmail.com>
2016-12-02 23:37:06 -0600

    basic JS module concepts.

---
+++ Learn and Understand NodeJS/greet.js
@@ -0,0 +1,9 @@
+/**
+ * Created by danny on 12/2/2016.
+ */
+
+var greet = function() {
+    console.log('Hello!');
+};
+
+module.exports = greet;
---
+++ Learn and Understand NodeJS/initialTest.js
@@ -0,0 +1,7 @@
+/**
+ * Created by Daniel A. Noventa on 12/2/2016.
+ */
+
+var greet = require('./greet');
+greet();
+
