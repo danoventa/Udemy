808b7877cd8584f9a58824610fa812f237c43eb1
Daniel Anthony Noventa <daniel.a.noventa@gmail.com>
2016-12-03 00:04:09 -0600

    Passing by reference vs. value.

--- Learn and Understand NodeJS/initialTest.js
+++ Learn and Understand NodeJS/initialTest.js
@@ -2,6 +2,51 @@
  * Created by Daniel A. Noventa on 12/2/2016.
  */
 
+// pass by value
+function change(b){
+    b = 2;
+}
+
+var a = 1;
+change(a);
+console.log(a)
+
+// pass by reference
+function changeObj(d){
+    d.prop1 = function(){};
+    d.prop2 = {};
+};
+var c = {};
+c.prop1 = {};
+changeObj(c);
+console.log(c);
+
+
+// Function to bject and prototype inheritance example
+function Person(firstname, lastname){
+    this.firstname = firstname;
+    this.lastname = lastname;
+}
+
+Person.prototype.greet = function(){
+    console.log('Hello, ' + this.firstname + ' ' + this.lastname);
+}
+
+var john = new Person('Jane', 'GI');
+john.greet();
+
+// example of object.
 var greet = require('./greet');
 greet();
 
+var person = {
+    firstname: 'John',
+    lastname: 'Doe',
+    greet: function(){
+        console.log('Hello, ' + this.firstname + ' ' + this.lastname);
+    }
+
+};
+
+person.greet();
+console.log(person['firstname']);
