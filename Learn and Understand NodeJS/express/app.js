d92bbc4e069986bdcf2a72a85f27be0a2413dca9
Daniel Anthony Noventa <daniel.a.noventa@gmail.com>
2016-12-13 23:52:57 -0600

    more middleware and json.

--- Learn and Understand NodeJS/express/app.js
+++ Learn and Understand NodeJS/express/app.js
@@ -1,20 +1,40 @@
 var express = require('express');
 var app = express();
+var bodyParser = require('body-parser');
+app.set('view engine', 'ejs');
 
 var port = process.env.PORT || 3000;
+var urlencodedParser = bodyParser.urlencoded({extended: false});
+var jsonParser = bodyParser.json();
 
 app.use('/assets', express.static(__dirname + '/public'));
+app.get('/', function(request, response){
+    response.render('index');
+});
+
+app.get('/person/:id', function(request, response){
+    response.render('person', {ID: request.params.id, Qstr: request.query.qstr});
+
+});
+
+app.post('/person', urlencodedParser, function(request, response){
+    response.send('thank you!');
+    console.log(request.body.firstname);
+    console.log(request.body.lastname);
+});
+
+app.post('/personjson', jsonParser, function(request, response){
+    response.send('Thank you for the JSON data!');
+    console.log(request.body.firstname);
+    console.log(request.body.lastname);
+});
 
 app.get('/', function(request, response){
-    response.send("<html><head><body><h1>Hellow Woors!</h1></body></head></html>")
+    response.send("")
 });
 
 app.get('/api', function(request, response){
     response.json({firstname:"john", lastname: "do"})
-});
-
-app.get('/person/:id', function(request, response){
-    response.send("<html><head><link href=assets/style.css type=text/css rel=stylesheet ><body><h1>Person: " + request.params.id + "</h1></body></link></head></html>")
 });
 
 app.listen(port);
