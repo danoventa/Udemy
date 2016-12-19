8a5c86587631e5c60609ebe757eca5056d7ad506
Daniel Anthony Noventa <daniel.a.noventa@gmail.com>
2016-12-18 23:41:13 -0600

    new es6 project.

---
+++ ES6JavaScript/start.js
@@ -0,0 +1,11 @@
+/**
+ * Created by danny on 12/18/2016.
+ */
+
+var number = 10;
+console.log(number / 20);
+console.log('your number is: ' + number);
+
+var greeting = {hello: 'there'};
+
+console.log('hellow ' + greeting.hello);
