426a7f7d87b4178b5331f322d4716bc4f87cdb7e
Daniel Anthony Noventa <daniel.a.noventa@gmail.com>
2017-01-10 08:48:13 -0600

    using every and some in validation case.

--- ES6JavaScript/everysome.js
+++ ES6JavaScript/everysome.js
@@ -1,3 +1,48 @@
+function Field(value){
+    this.value = value;
+}
+
+Field.prototype.validate = function(){
+    return this.value.length > 0;
+}
+
+var username = new Field('2cool');
+var password = new Field('my_password');
+var birthdate =- new Field('10/10/2016');
+
+var fields = [username, password, birthdate];
+
+var formIsValid = fields.every(function(field){
+    return field.validate();
+});
+
+if(formIsValid){
+    console.log("OK!");
+}else{
+    console.log("error");
+}
+
+
+
+var names = [
+    'alexandrai',
+    'mathew',
+    'joe'
+];
+
+var anna = names.every(function(name){
+    return name.length > 4;
+});
+
+var sona = names.some(function(name){
+    return name.length > 4;
+});
+
+console.log(sona);
+console.log(anna);
+
+
+
 var computers = [
     {name: 'Apple', ram: 24},
     {name: 'Compaq', ram: 3},
@@ -9,7 +54,7 @@ var someCan = false;
 
 for (var i = 0; i < computers.length; i++){
     var computer = computers[i];
-    
+
     if (computer.ram < 16) {
         allCan = false;
     }else{
