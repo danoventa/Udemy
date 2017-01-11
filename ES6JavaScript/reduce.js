62de9a1c23d5829d9cae4716fbb926b5c9bad5fb
Daniel Anthony Noventa <daniel.a.noventa@gmail.com>
2017-01-11 06:27:12 -0600

    balanced parenthesis with reduce.

--- ES6JavaScript/reduce.js
+++ ES6JavaScript/reduce.js
@@ -1,3 +1,16 @@
+function balancedParens(string){
+    return string.split("").reduce(function(previous, char){
+        if(previous < 0) {return previous}
+        if(char==="("){return ++previous;}
+        if(char===")"){return --previous;}
+
+        return previous;
+    }, 0);
+};
+
+console.log(balancedParens('((((( )))))'));
+
+
 var primaryColors = [
     {color: 'red'},
     {color: 'yellow'},
