f3a4611faf6e771c67770a8457348914597ffee7
Daniel Anthony Noventa <daniel.a.noventa@gmail.com>
2017-01-12 07:04:01 -0600

    using arrow functions.

---
+++ ES6JavaScript/arrowfunc.js
@@ -0,0 +1,17 @@
+const numbers = [1, 2, 3];
+
+numbers.map((number) 2 * number;);
+
+const double = number => 3 + number;
+console.log(double(1));
+
+const add = (a, b) => {
+    return a+b;
+};
+
+console.log(add(1, 2));
+
+const addz = (a, b) => a+b;
+
+console.log(addz(2, 3));
+
