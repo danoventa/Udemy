6c50b09fe3227b35e10bc2e79d4de2aa921fd772
Daniel Anthony Noventa <daniel.a.noventa@gmail.com>
2017-01-11 22:09:53 -0600

    using const and let in es6.

---
+++ ES6JavaScript/constletvar.js
@@ -0,0 +1,18 @@
+var color = 'red';
+console.log(color);
+
+var name = 'Jane';
+var title = 'Software Engineer';
+var hourlyWage = 40;
+
+//es6 features
+
+const nameu = 'Jane';
+let titleu = 'Software Engineer';
+titleu = 'Senior Software Engineer';
+
+let hourlyWageu = 40;
+hourlyWageu = 50;
+console.log(titleu);
+console.log(hourlyWageu);
+
