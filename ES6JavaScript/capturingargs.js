f4db5b7827257a1e767bac34e61c6964089ed0a8
Daniel Anthony Noventa <daniel.a.noventa@gmail.com>
2017-01-14 09:40:33 -0600

    using spread operator to join arrays.

--- ES6JavaScript/capturingargs.js
+++ ES6JavaScript/capturingargs.js
@@ -2,6 +2,14 @@
  * Created by danny on 1/14/2017.
  */
 
+const defaultColors = ['red', 'green'];
+const userFavoriteColors = ['organge','yellow'];
+const fallColors = ['fire red', 'fall orange'];
+
+
+console.log(['blue',...fallColors,...defaultColors, ...userFavoriteColors]);
+
+
 function addNumbers(...numbers){
     return numbers.reduce((sum, number) => {
         return sum + number;
