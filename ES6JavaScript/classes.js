5c7afc2c83aacbac64d9c9ba838bc5c0ad9de0a6
Daniel Anthony Noventa <daniel.a.noventa@gmail.com>
2017-01-18 07:42:23 -0600

    inheritance with es6

--- ES6JavaScript/classes.js
+++ ES6JavaScript/classes.js
@@ -9,9 +9,24 @@ class Card {
     }
 }
 
-const card = new Card( {title:'Visa'});
+class RewardsCard extends Card{
+    constructor(options){
+        super(options);
+        this.color = options.color;
+    }
+    rewards(){
+        return "points";
+    }
+}
+
+const card = new Card( {title:'Viso'});
 console.log(card);
 console.log(card.charge());
+
+const reCard = new RewardsCard({color:'blu',title:'Viso'});
+console.log(reCard);
+console.log(reCard.rewards());
+
 
 // Pre ES6
 function Car(options){
