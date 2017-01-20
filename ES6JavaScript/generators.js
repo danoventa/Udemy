d106ee4261e0a7cbeca40468503e5d630bea4bbc
Daniel Anthony Noventa <daniel.a.noventa@gmail.com>
2017-01-19 22:46:02 -0600

    generators.

--- ES6JavaScript/generators.js
+++ ES6JavaScript/generators.js
@@ -1,5 +1,17 @@
+function* colors(){
+    yield 'red';
+    yield 'blue';
+    yield 'green'
+}
+
+const gen2 = colors();
+console.log(gen2.next());
+console.log(gen2.next());
+console.log(gen2.next());
+console.log(gen2.next());
+
 function* numbers (){
-    yield;
+    yield '2';
 }
 const gen = numbers()
 console.log(numbers());
@@ -7,8 +19,8 @@ console.log(gen.next());
 console.log(gen.next());
 console.log(gen.next());
 
-const colors = ['red', 'green', 'blue'];
+const colorZ = ['red', 'green', 'blue'];
 
-for(let color of colors){
+for(let color of colorZ){
     console.log(color);
 }
