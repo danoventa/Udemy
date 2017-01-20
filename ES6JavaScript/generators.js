09bdb029df3d2a2fbd12425c0c1055b65b0244f8
Daniel Anthony Noventa <daniel.a.noventa@gmail.com>
2017-01-19 22:26:18 -0600

    Generators.

--- ES6JavaScript/generators.js
+++ ES6JavaScript/generators.js
@@ -1,4 +1,11 @@
-
+function* numbers (){
+    yield;
+}
+const gen = numbers()
+console.log(numbers());
+console.log(gen.next());
+console.log(gen.next());
+console.log(gen.next());
 
 const colors = ['red', 'green', 'blue'];
 
