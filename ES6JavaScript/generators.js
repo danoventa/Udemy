65796094030e6ee8412ccd0ddeeae881d57df8b7
Daniel Anthony Noventa <daniel.a.noventa@gmail.com>
2017-01-20 23:31:50 -0600

    iterators within classes/

--- ES6JavaScript/generators.js
+++ ES6JavaScript/generators.js
@@ -3,6 +3,13 @@ class Comment {
         this.content = content;
         this.children = children;
     }
+
+    *[Symbol.iterator](){
+        yield this.content;
+        for (let child of this.children){
+            yield* child;
+        }
+    }
 }
 
 const children = [
@@ -13,7 +20,12 @@ const children = [
 
 const tree = new Comment('Great post!', children);
 
-console.log(tree);
+const values = [];
+for(let value of tree){
+    values.push(value)
+};
+
+console.log(values);
 
 const testingTeam = {
     lead: 'Amanda',
