0b2a8c0ad77e467dea06daf931a4e607618a0948
Daniel Anthony Noventa <daniel.a.noventa@gmail.com>
2017-01-20 23:28:00 -0600

    tree data structure

--- ES6JavaScript/generators.js
+++ ES6JavaScript/generators.js
@@ -1,3 +1,20 @@
+class Comment {
+    constructor(content, children){
+        this.content = content;
+        this.children = children;
+    }
+}
+
+const children = [
+    new Comment('good comment', []),
+    new Comment('bad comment', []),
+    new Comment('meh', [])
+]
+
+const tree = new Comment('Great post!', children);
+
+console.log(tree);
+
 const testingTeam = {
     lead: 'Amanda',
     tester: 'Bill',
