6effa70962607e4cd20e91a1f5de4fa4522583c8
Daniel Anthony Noventa <daniel.a.noventa@gmail.com>
2017-01-23 22:55:21 -0600

    functions using the arrow.

--- AcceleratedES6/Changes.js
+++ AcceleratedES6/Changes.js
@@ -1,3 +1,16 @@
+
+// new syntax to write functions
+
+
+var fn = (a, b) => a + b;
+console.log(fn(1, 2))
+
+fn = () => {
+    console.log('hello!');
+};
+fn();
+
+// let and const vs var
 let age = 30;
 if(true){
     let age = 25;
