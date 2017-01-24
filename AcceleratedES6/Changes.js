59c3ccd65e57a5a47c78abf20a8ba3eafbe9f3c9
Daniel Anthony Noventa <daniel.a.noventa@gmail.com>
2017-01-23 22:50:44 -0600

    changes between es5 and 6.

---
+++ AcceleratedES6/Changes.js
@@ -0,0 +1,12 @@
+let age = 30;
+if(true){
+    let age = 25;
+    console.log(age);
+};
+console.log(age);
+
+// Scope of variables, var wouldn't do this.
+
+const agze = 90;
+// agze = 90 would error out, age is now a constant.
+console.log(agze);
