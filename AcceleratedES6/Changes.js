02f0c34d05e77b6113ac8aa60c1fcc2ac154de16
Daniel Anthony Noventa <daniel.a.noventa@gmail.com>
2017-01-24 23:01:54 -0600

    more rest examples.

--- AcceleratedES6/Changes.js
+++ AcceleratedES6/Changes.js
@@ -1,3 +1,16 @@
+// rest
+let numbers = [1, 2, 3,4 ,5 ];
+
+function setUp(...toAdd){
+    let result = 0;
+    for (let i = 0; i < toAdd.length; i++){
+        result += toAdd[i];
+    }
+    return result;
+}
+console.log(setUp(numbers));
+
+// object literals
 let name = 'Anna';
 let aje = 25;
 
