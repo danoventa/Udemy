aa18fd20d27fc0fd7660d5de9de7fbfe1ddf6e11
Daniel Anthony Noventa <daniel.a.noventa@gmail.com>
2017-02-03 23:08:00 -0600

    completed promises section with error checking.

--- AcceleratedES6/promises.js
+++ AcceleratedES6/promises.js
@@ -1,27 +1,32 @@
-
-
-// chained promises
+// chained promises, with error
 function waitASecond(seconds){
     return new Promise(function(resolve, reject){
-        setTimeout(function(){
-            seconds++;
-            resolve(seconds);
-        }, 10000);
+        if(seconds >2){
+            reject('Rejected');
+        } else {
+            setTimeout(function () {
+                seconds++;
+                resolve(seconds);
+            }, 1000);
+        }
     });
 }
 // runs twice
-waitASecond(0)
+waitASecond(1)
     .then(waitASecond)
     .then(function(seconds){
         console.log(seconds);
     });
 // runs thrice
-waitASecond(0)
+waitASecond(1)
     .then(waitASecond)
     .then(waitASecond)
     .then(function(seconds){
         console.log(seconds);
     })
+    .catch(function(error){
+        console.log(error);
+    });
 
 // basics
 let promise = new Promise(function(resolve, reject){
